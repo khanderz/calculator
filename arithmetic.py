95e3c5dac844b7a1d152fd302ccb28c5067c233c
Khanh Mai <khanhmai@creighton.edu>
2021-04-28 17:01:06 +0000

    lunch break

--- arithmetic.py
+++ arithmetic.py
@@ -1,35 +1,40 @@
 """Functions for common math operations."""
 
-
 def add(num1, num2):
     """Return the sum of the two inputs."""
 
-    return 10
+    return num1 + num2
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
+    return num1 - num2
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
+    return num1 * num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
+    return num1 / num2
 
 def square(num1):
     """Return the square of the input."""
 
+    return num1 ** 2
 
 def cube(num1):
     """Return the cube of the input."""
 
+    return num1 ** 3
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    return num1 ^ num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
